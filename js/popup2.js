a8679588757a1e91b7cb9e3560ae6a953e2b1cba
sergeyandmonique <136182797+sergeyandmonique@users.noreply.github.com>
2023-10-24 16:38:49 +0300

    Add files via upload

---
+++ js/popup2.js
@@ -0,0 +1,84 @@
+$('.photo-gallery-popup1').magnificPopup({    
+     items: {
+      src: '#magnific-gallery1',
+      type: 'inline'
+    },
+
+     closeMarkup:"<button title='%title%' type='button' class='mfp-close product-popup-close'></button>",
+        midClick: true,
+        focus: '#productPhotos',
+        overflowY: 'auto',
+        closeBtnInside: true,
+        preloader: false,      
+        midClick: true,
+        removalDelay: 300,
+        mainClass: 'my-mfp-zoom-in',
+        callbacks: {
+            open: function() {
+        
+       $('.gallery-slider').slick('setPosition');
+       $('.thumb-slider').slick('setPosition');
+           }
+   }
+});
+
+$('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+    $('.thumb-slider').slick('setPosition');
+});            
+
+$(document).ready(function() {
+  $('.popup-with-zoom-anim').magnificPopup({
+    type: 'inline',
+    fixedContentPos: false,
+    fixedBgPos: true,
+    overflowY: 'auto',
+    closeBtnInside: true,
+    preloader: false,
+    midClick: true,
+    removalDelay: 300,
+    mainClass: 'my-mfp-zoom-in'
+  });
+ });
+
+ $(document).ready(function(){
+        $('.gallery-slider').slick({
+            centerMode: true,
+            focusOnSelect: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: true,
+            fade: true,
+            asNavFor: '.thumb-slider'
+          });
+  
+        $('.thumb-slider').slick({
+            slidesToShow: 6,
+            slidesToScroll: 1,
+            asNavFor: '.gallery-slider',
+            dots: false,
+            arrows: true,
+            centerMode: false,
+            centerPadding: '20px',
+            focusOnSelect: true,
+            responsive: [
+              {
+                breakpoint: 450,
+                settings: {
+                  slidesToShow: 1
+                }
+              },
+              {
+                breakpoint: 700,
+                settings: {
+                  slidesToShow: 2
+                }
+              },
+              {
+                breakpoint: 1500,
+                settings: {
+                  slidesToShow: 3
+                }
+              }                   
+            ]
+        });
+      });
